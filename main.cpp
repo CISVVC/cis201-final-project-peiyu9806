34c8018d400225d01d2e5d186ab7d78a7c53c1b1
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-12-15 20:13:01 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+/**
+File: main.cpp
+Description:This program will find the words' concordances from the attach text file and print the result.
+Author: Pei-Yu Kuo
+Email: peiyuk9810@student.vvc.edu
+Data Created: 12-04-18
+**/
 #include<iostream>
-
 #include "concordance.h"
 
  
