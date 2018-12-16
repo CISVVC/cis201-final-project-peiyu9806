87a2872e2b96e47a858bc50a940f9a462dd66e39
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-12-16 00:02:48 -0800

    Update

--- concordance.h
+++ concordance.h
@@ -14,11 +14,15 @@ public:
     std::string next_word(std::ifstream &input);
     bool is_whitespace(char c);
     void eat_whitespace(std::ifstream &input);
-    int find_word(std::string word);
-    void print();
+    int find_word(std::string word) const;
+    void print() const;
+    void add_word(std::string word,int line);
+    bool is_poun(char c);
+    void eat_poun(std::ifstream &input);
 private:
     std::vector<Word> m_word_stats;
     std::string m_filename;
+    int m_line;
 };
 
 #endif
